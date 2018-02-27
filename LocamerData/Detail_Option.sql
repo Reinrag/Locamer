9ea06362da9bb3e2a77d78929b94d2a28f79d057
charry.clement <charry.clement@R20919>
2018-02-27 14:31:19 +0100

    Création des table Option et détail option

---
+++ LocamerData/Detail_Option.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Detail_Option]
+(
+	  [codeReservation] INT NOT NULL,
+    [codeOption]        INT NOT NULL,
+    [qteDemande]     INT NOT NULL,
+    [dureeOption]    INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([codeReservation] ASC),
+	PRIMARY KEY CLUSTERED ([codeOption] ASC),
+    CONSTRAINT [FK_dbo.Detail_Option_dbo.Reservation_codeReservation] FOREIGN KEY ([codeReservation]) 
+        REFERENCES [dbo].[Reservation] ([codeReservation]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.Detail_Option_dbo.Option_codeOption] FOREIGN KEY ([codeOption]) 
+        REFERENCES [dbo].[Option] ([codeOption]) ON DELETE CASCADE
+)
+
---
+++ LocamerData/Option.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Option]
+(
+	[codeOption]      INT           IDENTITY (1, 1) NOT NULL,
+    [libelleOption]       NVARCHAR (50) NULL,
+    [qteDispo]      INT  NULL
+    PRIMARY KEY CLUSTERED ([codeOption] ASC)
+)
