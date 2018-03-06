4049951fa8dbb2d563a1b3cd99dc84d710a22c3c
charry.clement <charry.clement@R20919>
2018-03-06 15:53:24 +0100

    Clréation de la classe métadata

---
+++ LocamerSite/Models/Metadata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LocamerSite.Models
+{
+    public class ReservationsMetadata
+    {
+        [StringLength(50)]
+        [Display(Name = "Nom du client")]
+        public string codeClient;
+
+        [Display(Name = "Date début de séjour")]
+        public Nullable<System.DateTime> dateDebut;
+
+        [StringLength(50)]
+        [Display(Name = "Date de fin de séjour")]
+        public Nullable<System.DateTime> dateFin;;
+
+        [StringLength(50)]
+        [Display(Name = "Type de séjour")]
+        public string codeSejour;
+    }
+   
+}
---
+++ LocamerSite/Models/PartialClasses.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LocamerSite.Models
+{
+    [MetadataType(typeof(ReservationsMetadata))]
+    public partial class Reservation
+    {
+    }
+}
